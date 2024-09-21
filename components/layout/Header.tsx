5ac7f05614b1704358893e2eeb23189b28ed8598
timothyshen <timothyshen1@hotmail.com>
2024-09-22 00:03:27 +0800

    refactor: top nav style and layout

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,19 +1,40 @@
+"use client";
+
 import Link from "next/link";
-import { Skull } from "lucide-react";
+import { Skull, Wallet, User } from "lucide-react";
 import { DynamicWidget } from "@dynamic-labs/sdk-react-core";
+import { useAccount } from "wagmi";
 
 export function Header() {
+  const { address, isConnected } = useAccount();
+
+  const sliceAddress = address?.slice(0, 6) + "..." + address?.slice(-4);
+
   return (
-    <header className="sticky top-0 z-10 bg-black/70 border-b border-amber-900/50">
+    <header className="fixed top-0 left-0 right-0 z-10 bg-transparent">
       <nav className="container mx-auto px-4 py-3 flex justify-between items-center">
-        <Link
-          href="/"
-          className="text-amber-500 text-xl font-bold flex items-center"
-        >
-          <Skull className="mr-2 h-6 w-6" />
-          DarkLegacyShop
-        </Link>
-        <DynamicWidget />
+        {isConnected ? (
+          <>
+            <div className="flex items-center space-x-2">
+              <Wallet />
+              <p>{sliceAddress}</p>
+            </div>
+            <Link href={`/user/${address}`}>
+              <User />
+            </Link>
+          </>
+        ) : (
+          <>
+            <Link
+              href="/"
+              className="text-amber-500 text-xl font-bold flex items-center"
+            >
+              <Skull className="mr-2 h-6 w-6" />
+              DarkLegacyShop
+            </Link>
+            <DynamicWidget />
+          </>
+        )}
       </nav>
     </header>
   );
--- components/page/HomeContent.tsx
+++ components/page/HomeContent.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Card,
   CardContent,
@@ -7,8 +9,14 @@ import {
 } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { useDynamicContext } from "@dynamic-labs/sdk-react-core";
+import { useAccount } from "wagmi";
+import { Wallet } from "lucide-react";
 
 export function HomeContent() {
+  const { isConnected } = useAccount();
+  const { setShowAuthFlow } = useDynamicContext();
+
   return (
     <main className="flex-grow container mx-auto px-4 py-8 flex flex-col items-center justify-center bg-[url('/images/background-image.png')] bg-cover bg-center">
       <h1 className="sticky top-0 z-10 text-2xl md:text-4xl font-bold text-pink-200 mb-8 shadow-pink-200/50 drop-shadow-md py-4">
@@ -22,28 +30,25 @@ export function HomeContent() {
             </CardTitle>
           </CardHeader>
           <CardContent className="flex justify-center bg-white rounded-b-lg overflow-hidden">
-            <Link href="/tasks" passHref>
+            {isConnected ? (
+              <Link href="/tasks">
+                <Button
+                  size="lg"
+                  className="text-xl px-12 py-8 bg-pink-300 hover:bg-pink-200 text-gray-800 rounded-full shadow-lg shadow-[#f3b8e1]/50 transform transition-all duration-200 hover:scale-105"
+                >
+                  Go to Tasks
+                </Button>
+              </Link>
+            ) : (
               <Button
                 size="lg"
                 className="text-xl px-12 py-8 bg-pink-300 hover:bg-pink-200 text-gray-800 rounded-full shadow-lg shadow-[#f3b8e1]/50 transform transition-all duration-200 hover:scale-105"
+                onClick={() => setShowAuthFlow(true)}
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-8 w-8 inline-block mr-2"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z"
-                  />
-                </svg>
+                <Wallet className="h-8 w-8 inline-block mr-2" />
                 Connect Wallet
               </Button>
-            </Link>
+            )}
           </CardContent>
           <CardFooter className="text-white p-4 text-center text-sm border-t-0">
             <p className="mx-auto">
