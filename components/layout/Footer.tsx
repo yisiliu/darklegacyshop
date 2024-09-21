b7f737e27508996e8955356a9117e1db291ffe32
Haoran Yu <haoran_yu@hotmail.com>
2024-09-21 13:08:19 +0800

    feat: update home screen to expected design

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -2,10 +2,10 @@ export function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="bg-black/70 border-t border-amber-900/50 text-amber-500 py-4">
-      <div className="container mx-auto px-4 text-center">
-        <p>&copy; {currentYear} DarkLegacyShop. All souls reserved.</p>
-      </div>
+    <footer className="border-t text-white py-4 bg-[#13385D] text-sm">
+      <p className="text-center">
+        &copy; {currentYear} DarkLegacyShop. All souls reserved.
+      </p>
     </footer>
   );
 }
