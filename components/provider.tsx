fa432473a70c6c173b578adc9b555056a0f2a159
timothyshen <timothyshen1@hotmail.com>
2024-09-21 12:38:07 +0800

    feat: adding dynamic as login method

--- components/provider.tsx
+++ components/provider.tsx
@@ -1,37 +1,36 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import {
-  DynamicContextProvider,
-  DynamicWidget,
-} from "@dynamic-labs/sdk-react-core";
+"use client";
+
+import { DynamicContextProvider } from "@dynamic-labs/sdk-react-core";
+import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
 import { DynamicWagmiConnector } from "@dynamic-labs/wagmi-connector";
 import { createConfig, WagmiProvider } from "wagmi";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { http } from "viem";
-import { mainnet } from "viem/chains";
-import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
+import { sepolia } from "viem/chains";
 
 const config = createConfig({
-  chains: [mainnet],
+  chains: [sepolia],
   multiInjectedProviderDiscovery: false,
   transports: {
-    [mainnet.id]: http(),
+    [sepolia.id]: http(),
   },
 });
 
 const queryClient = new QueryClient();
 
-export default function WagmiProviderWrapper({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function App({ children }: { children: React.ReactNode }) {
   return (
     <DynamicContextProvider
       settings={{
         environmentId: "711895d2-a81c-462e-885e-409997a413f0",
+        walletConnectors: [EthereumWalletConnectors],
       }}
     >
-      {children}
+      <WagmiProvider config={config}>
+        <QueryClientProvider client={queryClient}>
+          <DynamicWagmiConnector>{children}</DynamicWagmiConnector>
+        </QueryClientProvider>
+      </WagmiProvider>
     </DynamicContextProvider>
   );
 }
